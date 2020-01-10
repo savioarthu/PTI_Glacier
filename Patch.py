480baea7f71ca0da79cfe07ec37a4aa48780a00c
Felix <felix.cloup@eisti.fr>
2020-01-10 10:07:30 +0100

    Patch class done

--- Patch.py
+++ Patch.py
@@ -6,61 +6,46 @@ colors = [0, 1, 2]
 
 
 class Patch():
-	def __init__(self, im, x=None, y=None, size_patch=20):
-		if type(im) is list:
-			self.nb_images = len(im)
-			self.image_index = randint(0, self.nb_images - 1)
-			self.im = im[self.image_index]
+	def __init__(self, image, x=None, y=None, size_patch=20):
+		if type(image) is list:
+			nb_images = len(image)
+			self.image_index = randint(0, nb_images - 1)
+			self.image = image[self.image_index]
 		else:
-			self.nb_images, self.image_index = 0, 0
-			self.im = im
+			self.image_index = 0
+			self.image = image
 
-		self.nb_images = len(im) if type(im) is list else 0
+		self.image_shape = np.shape(self.image)
 
 		self.size = size_patch
-		im_size = self.im.shape
-
 		self.x = x if ((x is not None) and (x >= 0) and (
-			x <= im_size[0] - self.size)) else randint(0, im_size[0] - self.size)
+			x <= self.image_shape[0] - self.size)) else randint(0, self.image_shape[0] - self.size)
 		self.y = y if ((y is not None) and (y >= 0) and (
-			y <= im_size[1] - self.size)) else randint(0, im_size[1] - self.size)
+			y <= self.image_shape[1] - self.size)) else randint(0, self.image_shape[1] - self.size)
 
-		self.im = self.im[self.x:self.x +
-						  self.size, self.y:self.y + self.size, :]
-		self.image_to_patch()
+		self.image = self.image[self.x:self.x +
+								self.size, self.y: self.y + self.size, :]
+		self.image_to_vector()
 
-		self.hists = [np.histogram(self.im[color], bins=np.arange(-2, 2, 0.1), density=True)[
+		self.hists = [np.histogram(self.vector[color], bins=np.arange(-2, 2, 0.1), density=True)[
 			0] for color in colors]
 
-	def image_to_patch(self):
-		self.mean = [self.im[:, :, color].mean() for color in colors]
-		self.std = [self.im[:, :, color].std() for color in colors]
-		self.im = [np.reshape((self.im[:, :, color] - self.mean[color]) /
-							  self.std[color], (1, self.size ** 2)) for color in colors]
+	def image_to_vector(self):
+		vector = self.image
+		self.mean = [vector[:, :, color].mean() for color in colors]
+		self.std = [vector[:, :, color].std() for color in colors]
+		self.vector = [n((vector[:, :, color] - self.mean[color]) /
+						 self.std[color], (1, self.size ** 2)) for color in colors]
 
-	def patch_to_image(self, uncentered=True):
-		if uncentered:
-			image = [[pixR * self.std[0] + self.mean[0], pixG * self.std[1] + self.mean[1], pixB *
-					 self.std[2] + self.mean[2]] for pixR, pixG, pixB in zip(self.im[0], self.im[1], self.im[2])]
-		else:
-			print(np.shape(self.im))
-			image = [[pixR, pixG, pixB] for pixR, pixG, pixB in zip(self.im[0], self.im[1], self.im[2])]
-			print(image)
-		return np.reshape(image, (self.size, self.size, 3))
+	def vector_to_image(self):
+		image = [[(pixR * self.std[0] + self.mean[0]) / 256,
+				  (pixG*self.std[1] + self.mean[1]) / 256,
+				  (pixB*self.std[2] + self.mean[2]) / 256]
+				 for pixR, pixG, pixB in zip(self.vector[0][0], self.vector[1][0], self.vector[2][0])]
+		self.image = n(image, (self.size, self.size, 3))
 
 	def show(self, im=None):
 		print(f"x: {self.x}\ty: {self.y}\timage: {self.image_index}")
-		reshaped_im = self.patch_to_image(False)
-		if im is not None:
-			base_im = im[self.image_index][self.x:self.x +
-										   self.size, self.y:self.y + self.size, :] / 256
-		else:
-			base_im = self.patch_to_image()
-		# image = np.concatenate(
-		# 	(reshaped_im, base_im), axis=1)
-		image = base_im
-		print(np.shape(self.im))
-		print(self.im)
-		print(np.shape(base_im))
-		print(base_im)
-		cv2.imshow(f'patch{self.x}_{self.y}_image{self.image_index}', image)
+		self.vector_to_image()
+		cv2.imshow(
+			f'patch{self.x}_{self.y}_image{self.image_index}', self.image)
--- test_Patch.py
+++ test_Patch.py
@@ -1,25 +1,30 @@
 from Patch_collection import Patch_collection
 import cv2
+import numpy as np
 
-paths = ['./Photos/2007060208_cam01.jpg', './Photos/2007060608_cam01.jpg', './Photos/2007060908_cam01.jpg', './Photos/2007061708_cam01.jpg']
+paths = ['./Photos/2007060208_cam01.jpg', './Photos/2007060608_cam01.jpg',
+		 './Photos/2007060908_cam01.jpg', './Photos/2007061708_cam01.jpg']
 paths_chat = ['./Photos_test/chat1.jpg', './Photos_test/chat2.jpg']
 
 
-images = [cv2.imread(path) for path in paths_chat]
+images = [cv2.imread(path) for path in paths]
 
 
-coll = Patch_collection(images, nb=10, size_patch=60)
-coll.reconstruct_image()
+coll = Patch_collection(images, nb=10, size_patch=600)
 
+# coll.select_patches(nb_iter=3, threshold=0.01, fill=False)
+print(0)
+coll.grid_patches()
+print(1)
+image = coll.reconstruct_image()
+print(2)
+cv2.imshow('image reconstructed', image)
 
+# for patch in coll.patches:
+# 	patch.show(corrected=True)
 
-# coll.select_patches(nb_iter=10, threshold=0.015, fill=False)
+while True:
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-# for patch in coll.patches[:10]:
-# 	patch.show(im=images)
-
-# while True:
-# 	if cv2.waitKey(1) & 0xFF == ord('q'):
-# 		break
-
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
