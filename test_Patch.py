4fb8cc1a98712e5f22c5c5a95c08b4989ba30560
Félix <felixcloup@gmail.com>
2020-03-02 15:10:25 +0100

    indentation

--- test_Patch.py
+++ test_Patch.py
@@ -5,7 +5,7 @@ import time
 
 start = time.time()
 paths = ['./Photos/2007060208_cam01.jpg', './Photos/2007060608_cam01.jpg',
-		 './Photos/2007060908_cam01.jpg', './Photos/2007061708_cam01.jpg']
+         './Photos/2007060908_cam01.jpg', './Photos/2007061708_cam01.jpg']
 paths_chat = ['./Photos_test/chat1.jpg', './Photos_test/chat2.jpg']
 
 
@@ -15,21 +15,21 @@ images = [cv2.imread(path) for path in paths]
 coll = Patch_collection(images, nb=1024, size_patch=32)
 
 # coll.select_patches(nb_iter=3, threshold=0.01, fill=False)
-#print(0)
-#coll.grid_patches()
-#print(1)
+# print(0)
+# coll.grid_patches()
+# print(1)
 #image = coll.reconstruct_image()
-#print(2)
+# print(2)
 #cv2.imshow('image reconstructed', image)
 
 # for patch in coll.patches:
 # 	patch.show(corrected=True)
 
-#while True:
+# while True:
 #	if cv2.waitKey(1) & 0xFF == ord('q'):
 #		break
 
-#cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 
 
 # Step 1 - Recover patches
@@ -37,7 +37,7 @@ coll.select_patches(nb_iter=3, threshold=0.01, fill=False)
 R, G, B = coll.separate_patches()
 
 # Step 2 - KSVD with these patches
-dict_size = 1100 #overcomplete
+dict_size = 1100  # overcomplete
 sparsity = 1
 D_R, Gamma_R = k_svd(R, dict_size, sparsity)
 D_G, Gamma_G = k_svd(G, dict_size, sparsity)
@@ -46,7 +46,7 @@ D_B, Gamma_B = k_svd(B, dict_size, sparsity)
 
 end = time.time()
 print("SUCCESS")
-print("Time :", round(end-start,3), "seconds")
+print("Time :", round(end-start, 3), "seconds")
 
 # Step 3 - Sort by decreasing variance the columns of D
 variance = np.var(D_R, axis=1)
@@ -69,27 +69,26 @@ noisy_coll.grid_patches()
 Rnoisy, Gnoisy, Bnoisy = noisy_coll.separate_patches()
 
 # Step 6 - Mean and std each patch and new xbi
-# ???? 
+# ????
 
 X_R = np.zeros((Rnoisy.shape[0], Rnoisy.shape[1]))
 X_G = np.zeros((Gnoisy.shape[0], Gnoisy.shape[1]))
 X_B = np.zeros((Bnoisy.shape[0], Bnoisy.shape[1]))
 # Step 7 - OMP(xbi, D) = alphabi
-for i in range(0,len(Rnoisy)):
-	alphaR = omp(D_R, Rnoisy[i].reshape((1024,1)), sparsity)
-	alphaG = omp(D_G, Gnoisy[i].reshape((1024,1)), sparsity)
-	alphaB = omp(D_B, Bnoisy[i].reshape((1024,1)), sparsity)
+for i in range(0, len(Rnoisy)):
+    alphaR = omp(D_R, Rnoisy[i].reshape((1024, 1)), sparsity)
+    alphaG = omp(D_G, Gnoisy[i].reshape((1024, 1)), sparsity)
+    alphaB = omp(D_B, Bnoisy[i].reshape((1024, 1)), sparsity)
 
-	# Step 8 - Dalphabi = Xfin
-	X_R[i] = np.matmul(D_R, alphaR).reshape(1024)
-	X_G[i] = np.matmul(D_G, alphaG).reshape(1024)
-	X_B[i] = np.matmul(D_B, alphaB).reshape(1024)
+    # Step 8 - Dalphabi = Xfin
+    X_R[i] = np.matmul(D_R, alphaR).reshape(1024)
+    X_G[i] = np.matmul(D_G, alphaG).reshape(1024)
+    X_B[i] = np.matmul(D_B, alphaB).reshape(1024)
 
 # Step 9 - Reconstruct
 # X_R X_G X_B de taille 17424 / 1024
-# Il faut reconstruire en un seul X -> ce que je fais déjà reconstruct_image. 
+# Il faut reconstruire en un seul X -> ce que je fais déjà reconstruct_image.
 # Il faut donc juste rentrer X_R X_G et X_B dans la classe ou faire differemment de ce que j'ia fait
 reconstruct_image = noisy_coll.reconstruct_image()
 cv2.imshow('image reconstructed', reconstruct_image)
 cv2.destroyAllWindows()
-
